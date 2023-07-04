c26c9e7d64bb0933bf715fabb59a1fc6bc161293
Adithya <bokkisum.adithya@sap.com>
2023-07-04 07:41:35 +0530

    Completing the project by class based components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import {Component} from "react"
-import './App.css';
-import CardList from "./components/card-list/card-list.component"
+import { Component } from "react";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component{
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters:[],
-      searchField: ""
+      monsters: [],
+      searchField: "",
     };
   }
   async componentDidMount() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const aUsers = await response.json();
-    this.setState(()=> {
+    // console.log(aUsers)
+    this.setState(() => {
       return {
         monsters: aUsers,
-        searchField: ""
-      }
-    })
+        searchField: "",
+      };
+    });
   }
-  onSearchChange = (event)=>{
+  onSearchChange = (event) => {
     const searchField = event.target.value.toLowerCase();
-    this.setState(()=>{
-      return {searchField}
-    })
-  }
+    this.setState(() => {
+      return { searchField };
+    });
+  };
   getSuggestedMonsters = () => {
-    const {monsters,searchField} = this.state
-    return monsters.filter((monster)=>monster.name.toLowerCase().includes(searchField));
-  }
+    const { monsters, searchField } = this.state;
+    return monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField)
+    );
+  };
   render() {
     console.log("render from App");
     const aSuggestedMonsters = this.getSuggestedMonsters();
     return (
       <div className="App">
-        <input className="search-box" placeholder="Search Monsters" type="search" onChange={this.onSearchChange}/>
-        <CardList monsters = {aSuggestedMonsters}/>
+        <h1 className="app-title">Monsters Cards</h1>
+        <SearchBox className="monsters-search-box" onChangeHandler={this.onSearchChange} placeholder="Search"/>
+        <CardList monsters={aSuggestedMonsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,21 @@
 import { Component } from "react";
+import Card from "../card/card.component";
+import "./card-list.styles.css"
 
 class CardList extends Component {
     render() {
         const {monsters} = this.props;
-        console.log("render from CardList");
-        // console.log(this.props.monsters);
         return(
-            <div>
-                {
-                    monsters.map((monster)=><div key={monster.id}>{monster.name}</div>)
-                }
-            </div>
+           <div className="card-list">
+           {
+               monsters.map((monster)=>{
+                const {id,name,email} = monster;
+                return(
+                    <Card id={id} name = {name} email = {email}/>
+                )
+               })
+           }
+           </div>
         )
     }
 }
